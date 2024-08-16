b0ca0f04bf817e7dc7eecf0e9e7087f081af6bba
jmsparrowk <jmsparrowk@naver.com>
2024-08-16 23:29:57 +0900

    8/16

---
+++ src/main/java/public_api/instargram_gide_book/InstargramGideBookApplication.java
@@ -0,0 +1,13 @@
+package public_api.instargram_gide_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramGideBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstargramGideBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/public_api/instargram_gide_book/InstargramGideBookApplicationTests.java
@@ -0,0 +1,13 @@
+package public_api.instargram_gide_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstargramGideBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
